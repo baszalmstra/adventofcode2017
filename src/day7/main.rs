17fc30ebd52092027e6858b1275c2eb105b0dd91
Bas Zalmstra <zalmstra.bas@gmail.com>
2017-12-16 14:46:55 +0100

    day7

--- src/day7/main.rs
+++ src/day7/main.rs
@@ -1,3 +1,5 @@
+#![feature(entry_and_modify)]
+
 use std::fs::File;
 use std::io::BufReader;
 use std::io::BufRead;
@@ -11,7 +13,8 @@ struct Tower {
     name: String,
     parent: Weak<RefCell<Tower>>,
     subs: Vec<Rc<RefCell<Tower>>>,
-    weight: u32
+    weight: u32,
+    total_weight: u32
 }
 
 impl Tower {
@@ -20,8 +23,40 @@ impl Tower {
             name: String::from(name),
             subs: Vec::new(),
             parent: Weak::new(),
-            weight: 0
+            weight: 0,
+            total_weight: 0,
         }
+    }
+
+    fn calc_total(&mut self) -> u32 {
+        self.total_weight = self.weight;
+        for sub in self.subs.iter() {
+            self.total_weight += sub.borrow_mut().calc_total();
+        }
+
+        self.total_weight
+    }
+
+    fn find_unbalanced_sub(&self) -> Option<Rc<RefCell<Tower>>> {
+        let values = self.sub_histogram();
+        if values.len() > 1 {
+            for (_, ref indices) in values.iter() {
+                if indices.len() == 1 {
+                    return Some(self.subs[indices[0]].clone());
+                }
+            }
+        }
+        None
+    }
+
+    fn sub_histogram(&self) -> HashMap<u32, Vec<usize>> {
+        let mut values:HashMap<u32, Vec<usize>> = HashMap::new();
+        for (index, sub) in self.subs.iter().enumerate() {
+            values.entry(sub.borrow().total_weight)
+                .and_modify(|indices| { (*indices).push(index); })
+                .or_insert(vec![index]);
+        }
+        values
     }
 }
 
@@ -68,6 +103,36 @@ fn main() {
     }
 
     // Find the root
-    let root = nodes.values().find(|tower| tower.borrow().parent.upgrade().is_none()).unwrap().borrow();
-    println!("Root: {}", root.name);
+    let root = nodes.values().find(|tower| tower.borrow().parent.upgrade().is_none()).unwrap();
+    {
+        let mut root_mut = root.borrow_mut();
+        println!("Root: {}", root_mut.name);
+
+        // Calc total weights
+        root_mut.calc_total();
+
+        
+    }
+
+    let mut unbalanced_sub = root.clone();
+    loop {
+        let unbalanced_child = unbalanced_sub.borrow().find_unbalanced_sub();
+        match unbalanced_child {
+            Some(child) => {
+                unbalanced_sub = child;
+            },
+            None        => {
+                let parent = unbalanced_sub.borrow().parent.upgrade().unwrap();
+                let histogram = parent.borrow().sub_histogram();
+                let (balanced_weight, _) = histogram.iter().find(|&(_,v)| v.len() > 1).unwrap();
+                let unbalanced_sub_borrow = unbalanced_sub.borrow();
+                println!("Unbalanced sub: {}, weight: {}, should be: {}", 
+                    unbalanced_sub_borrow.name,
+                    unbalanced_sub_borrow.weight,
+                    balanced_weight-(unbalanced_sub_borrow.total_weight-unbalanced_sub_borrow.weight));
+                break;
+            }
+        }
+        
+    };   
 }
